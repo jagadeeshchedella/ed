0f2e3067b3f3a5d32c96a26bc04b994bd9417b86
paytmuser <nath.shanmukha@gmail.com>
2019-09-11 20:15:02 +0530

    updated the error

--- db.java
+++ db.java
@@ -3,4 +3,12 @@
 good morning shanmukha
 this is a devupdated file
 
+<<<<<<< HEAD
 this is ......file
+=======
+this is ......fil
+ hi git stash command
+ ...updated file through git cherrypick
+
+ im updating it now
+>>>>>>> d342966... updated in db.java now
